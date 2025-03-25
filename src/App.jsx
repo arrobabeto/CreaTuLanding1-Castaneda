9005f6888ab00778e46e5d24295e6c78daa44d4a
Alberto Castañeda <alberto@Mac-mini-de-Alberto.local>
2025-03-24 18:37:51 -0600

    Entrega de la primera parte del proyecto: estructura de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import NavBar from "./components/NavBar";
-import ItemListContainer from "./components/ItemListContainer";
+import NavBar from './components/NavBar';
+import CartWidget from './components/CartWidget';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <CartWidget />
       <ItemListContainer mensaje="¡Bienvenido a nuestra tienda!" />
     </div>
   );
